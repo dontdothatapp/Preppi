d1ca485575bce7572e06bf9fbdd3e8cfb8d4a5a7
Stepan V <velichko.stepan@gmail.com>
2023-01-16 21:09:54 +0100

    Initial Commit

---
+++ Preppi/PreppiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreppiApp.swift
+//  Preppi
+//
+//  Created by Степан Величко on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PreppiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
